9f6d0ea40ecdeb203664655e3b11a9aa60c5423c
Jana Janković <jana.jankovic@student.um.si>
2023-03-12 15:12:17 +0100

    server changes

--- src/serve/server.py
+++ src/serve/server.py
@@ -3,6 +3,8 @@ import pandas as pd
 from flask import Flask
 from flask import request
 import os
+from datetime import datetime, timedelta
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -18,6 +20,7 @@ def reorder(data):
 
 
 @app.route('/air/predict/', methods=['POST'])
+@cross_origin()
 def predict():
     object_json = request.json
     df = reorder(object_json)
@@ -37,5 +40,42 @@ def predict():
     return json_data
 
 
+@app.route('/forecast', methods=['GET'])
+@cross_origin()
+def forecast():
+    root_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '../..'))
+    data_path = os.path.join(root_dir, 'data', 'processed', 'data.csv')
+    csv = pd.read_csv(data_path, encoding='utf_8')
+    df = pd.DataFrame(csv)
+
+    date_from = df['date'].tail(1).values[0]
+
+    df = df.drop(columns='date')
+    df = df.tail(72)
+
+    root_dir = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '../..'))
+    model_path = os.path.join(root_dir, 'models', 'var_model')
+
+    f = open(model_path, 'rb')
+    model = pickle.load(f)
+
+    predictions = model.forecast(df.values, steps=72)
+    predictions = pd.DataFrame(
+        predictions, columns=df.columns, index=df.index)
+
+    df_dict = predictions.to_dict()
+    json_data = {key: list(df_dict[key].values()) for key in df_dict}
+
+    start_date = datetime.strptime(date_from, '%Y-%m-%d %H:%M:%S')
+    start_date += timedelta(hours=1)
+    date_list = [start_date + timedelta(hours=i) for i in range(72)]
+    date_str_list = [date.strftime('%Y-%m-%d %H:%M:%S') for date in date_list]
+    json_data['date'] = date_str_list
+    return json_data
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
