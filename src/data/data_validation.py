deee9b0fd02c5ea56c41220f6486dcfca97394a6
Jana Janković <jana.jankovic@student.um.si>
2023-03-27 11:18:09 +0200

    update

--- src/data/data_validation.py
+++ src/data/data_validation.py
@@ -28,8 +28,8 @@ import os
 
 
 def main():
-    root_dir = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), '../..'))
+    root_dir = os.getcwd()
+    print(root_dir)
     path = os.path.join(root_dir, 'great_expectations')
 
     data_context: FileDataContext = get_context(
